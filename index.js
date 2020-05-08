31f5762a58049caab79d067c0eb86dec22ec6333
ljv29 <jumpswervelogan@gmail.com>
2020-05-08 14:46:04 -0700

    created questions array

---
+++ index.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        title: "The correct syntax for making a div is...",
+        choices: ["<div>", "<div></div>", "<div/>", "<div><div/>"],
+        answer: "<div></div>"
+    },
+    {
+        title: "when linking a stylesheet, its best to enclose in a _____ tag?",
+        choices: ["div", "a", "link", "p"],
+        answer: "link"
+    },
+    {
+        title: "when calling an id, you must use ____?",
+        choices: ["You can only call classes, not id's", "$", ".", "#"],
+        answer: "#"
+    },
+    {
+        title: "The best way to debug code as well as making sure youre succesful is by using which function?",
+        choices: ["console.log", "check.log", "writeToPage.log", "error.log"],
+        answer: "console.log"
+    },
+    {
+        title: "What's the popular coding acronym KISS stand for?",
+        choices: ["Keep It Simple Silly", "Keep It Simple Stupid", "Keep Inventing Suitable Syntax", "Keep Investing in Suitable Syntax"],
+        answer: "Keep It Simple Stupid"
+    }
+];
